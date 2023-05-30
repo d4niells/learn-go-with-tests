2a87b0e72af5b76601a35f8fb8e8a9623824edd6
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-05-30 08:47:25 -0300

    feat(pointers): create a bitcoin wallet to learn pointers

---
+++ pointers/wallet.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
---
+++ pointers/wallet_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
