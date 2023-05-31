5db0c2a9febf9eda3cc88f1763a3c5cd0bfba668
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-05-31 12:29:06 -0300

    feat(maps): delete words from dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -50,6 +50,10 @@ func (d Dictionary) Update(word, newDefinition string) error {
 	return nil
 }
 
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -71,6 +71,19 @@ func TestUpdate(t *testing.T)  {
 	})
 }
 
+func TestDelete(t *testing.T)  {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
