c37d35b7a92610697159200617839fda2896ed6b
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-05-31 12:22:52 -0300

    feat(maps): update word definition on the dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -7,6 +7,7 @@ type Dictionary map[string]string
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 func (d Dictionary) Search(word string) (string, error)  {
@@ -26,6 +27,22 @@ func (d Dictionary) Add(word, definition string) error {
 		d[word] = definition
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = newDefinition
 	default:
 		return err
 	}
