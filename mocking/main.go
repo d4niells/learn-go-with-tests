1e048d18dc252d14ed00789e9688902d2e936ad9
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-10-10 20:17:44 -0300

    feat(mocking): implement sleeper between each num

--- mocking/main.go
+++ mocking/main.go
@@ -7,29 +7,36 @@ import (
 	"time"
 )
 
-type DefaultSleeper struct{}
+// Sleeper allows you to put delays.
+type Sleeper interface {
+	Sleep()
+}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+// ConfigurableSleeper is an implementation of Sleeper with a defined delay.
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+// Sleep will pause execution for the defined Duration.
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-type Sleeper interface {
-	Sleep()
-}
-
+// Countdown prints a countdown from 3 to out with a delay between count provided by Sleeper.
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
-
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
