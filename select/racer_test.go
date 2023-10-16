e5b6288fda7e211af8328815ea7702a7ba554c34
Daniel Oliveira <danieloliver.dev@gmail.com>
2023-10-16 18:57:22 -0300

    feat(select): using select to wait on multiple channels

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,21 +8,40 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("compares speeds of servers, returning the url of the fastest one", func(t *testing.T)  {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+	
+		slowServer.Close()
+		fastServer.Close()
+	
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+	
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
+	
+		if err != nil {
+			t.Fatalf("did not expect an error but one %v", err)
+		}
 
-	slowServer.Close()
-	fastServer.Close()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		timeout := 20 * time.Millisecond
+		server := makeDelayedServer(25 * time.Millisecond)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		defer server.Close()
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		_, err := ConfigurableRacer(server.URL, server.URL, timeout)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
